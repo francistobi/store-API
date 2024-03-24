2c2d9a9812f69551af55080d583ab0c6121d9eb3
francistobi <frobigwe305@gmail.com>
2024-03-24 01:36:21 +0100

    new commit

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
-const app = express();
+const app = express()
 const connectDB = require("./db/connect");
 const url = process.env.MONGO_URL;
-const productRouter = require("./routes/products");
+const productRouter = require("./routes/products")
 
 const notFoundMiddleware = require("./middleware/not-found");
-const errorMiddleware = require("./middleware/error-handler");
+const errorMiddleware = require("./middleware/error-handler")
 
-app.use(express.json());
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send(`<h1>store api</h1> <a href="/api/v1/products">product route</a>`);
@@ -25,7 +25,7 @@ const port = process.env.PORT || 2000;
 
 const start = async () => {
   try {
-    await connectDB(url);
+    await connectDB(url)
     app.listen(port, () => {
       console.log(`server started successfully at port ${port}`);
     });
--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,17 @@
-
 const product = require("../models/product");
 
 const getAllProductStatic = async (req, res) => {
-  const products = await product.find({ name: "entertainment center" });
-  res.status(200).json({ products, nbHits: products.length });
+  const products = await product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length })
 };
 const getAllProduct = async (req, res) => {
-  const products = await product.find(req.query)
-  res.status(200).json({products, nbhits: products.length});
-};
+  const {featured} =req.query
+  const queryObigwe = {}
+  const products = await product.find(req.query);
+  res.status(200).json({ products, nbhits: products.length });
+}
 
 module.exports = {
   getAllProductStatic,
   getAllProduct,
-}
+};
