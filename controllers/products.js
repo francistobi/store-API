e5bf2a9db9105592d822971a3c5c44da1412cb34
francistobi <frobigwe305@gmail.com>
2024-03-28 03:33:19 +0100

    new commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,24 +1,27 @@
 const product = require("../models/product");
 
 const getAllProductStatic = async (req, res) => {
-  const products = await product.find(req.query);
-  res.status(200).json({ products, nbHits: products.length })
+  const search = "c";
+  const products = await product.find({
+    name: { $regex: search, $options: "i" },
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProduct = async (req, res) => {
-  const {featured, company, name} =req.query
-  const queryObject = {}
-  if(featured) {
-    queryObject.featured = featured === "true"? true :false
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
   }
-  if(company) {
-    queryObject.company = company
+  if (company) {
+    queryObject.company = company;
   }
-  if(name) {
-    queryObject.name = name;
+  if (name) {
+    queryObject.name = {$regex: name, $options: "i"};
   }
   const products = await product.find(queryObject);
   res.status(200).json({ products, nbhits: products.length });
-}
+};
 
 module.exports = {
   getAllProductStatic,
